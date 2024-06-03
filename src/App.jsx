8eb6385ca7d4e3ef84322f111ad93c9d0c513869
IMxSuhasTheBoy <dannylivecsr2@gmail.com>
2024-06-03 18:01:58 +0530

    Add react router dom test

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-// import React from 'react'
+import {
+  Route,
+  createRoutesFromElements,
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import HomeCards from "./components/HomeCards";
+import JobListings from "./components/JobListings";
+import ViewAllJobs from "./components/ViewAllJobs";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route index element={<h1>My app</h1>} />)
+);
 
 const App = () => {
-  return (
-    <>
-      <Navbar />
-    </>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
